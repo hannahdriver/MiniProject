19649f80a256b7a47333b9cc5c6dc1b71d71d75b
hannahdriver <36014350+hannahdriver@users.noreply.github.com>
2022-08-12 15:55:28 -0400

    Add execution statements
    Add execution statements for parts A, B and D of the main file

--- main.py
+++ main.py
@@ -8,19 +8,36 @@ from utils import *
 
 def main():
 
-    ###Part 1
-
     #Read in images and cast to Float32 type
     img = readImage("case23_resampled.nii")
     segment = readImage("case23_resampled_segmentation.nii")
-    img = sitk.Cast(img, sitk.sitkFloat32)
-    segment = sitk.Cast(segment, sitk.sitkFloat32)
+    img32 = sitk.Cast(img, sitk.sitkFloat32)
+    #segment32 = sitk.Cast(segment, sitk.sitkFloat32)
 
-    ###Part 2
+    ###Part A
+    #Filter image
+    filtered_img = preSegmentationFilter(img32, 11, 250, 445, 3, 12, 8)
 
-    ###Part 3
-    get_target_loc(segment)
+    #Set seeds for segmenting
+    fid1 = (181, 116, 32)
+    fid2 = (109, 195, 32)
+    fid3 = (133, 156, 32)
+    fid4 = (231, 186, 32)
 
-    ###Part 4
+    #Segment
+    segmented_prostate = prostate_segmenter(filtered_img, fid1, fid2, fid3, fid4, 50, 115)
+
+    #View overlayed segments (gold standard and newly generated segment)
+
+
+    ###Part B
+    seg_eval_dice(segment,segmented_prostate)
+
+    ###Part C
+    biopsy_coordinates = get_target_loc(segment,img)
+
+    ###Part D
+    pixel_extract(img,biopsy_coordinates,6)
+
 
 main()
